4936da16db7f8ae80f5f896852baa903e3274d06
kathmandu777 <mahhakatomanato@gmail.com>
2022-11-03 00:06:50 +0900

    fix

--- fastapi/gunicorn.conf.fastapi.py
+++ fastapi/gunicorn.conf.fastapi.py
@@ -2,7 +2,7 @@
 wsgi_app = "config.asgi:fastapp"
 worker_class = "config.worker.RestartableUvicornWorker"
 bind = "0.0.0.0:8000"
-workers = 1
+workers = 8
 reload = True
 daemon = True
-# forwarded_allow_ips = "*"
+forwarded_allow_ips = "*"
